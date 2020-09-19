71604feec6966fec1a9b6733912707d5167d463a
stepin84878 <69247364+stepin84878@users.noreply.github.com>
2020-09-19 06:49:18 +0530

    Add files via upload

---
+++ project_main.c
@@ -0,0 +1,85 @@
+#include "calculator_operations.h"
+int main()
+{
+    int X=1;
+    char Calc_oprn;
+ 
+    // Function call 
+    calculator_operations();
+ 
+    while(X)
+    {
+        printf("\n");
+        printf("%s : ", KEY);
+ 
+        Calc_oprn=getche();
+ 
+        switch(Calc_oprn)
+        {
+            case '+': addition();
+                      break;
+ 
+            case '-': subtraction();
+                      break;
+ 
+            case '*': multiplication();
+                      break;
+ 
+            case '/': division();
+                      break;
+ 
+            case '?': modulus();
+                      break;
+ 
+            case '!': factorial();
+                      break;
+ 
+            case '^': power();
+                      break;
+ 
+            case 'H':
+            case 'h': calculator_operations();
+                      break;
+ 
+            case 'Q':
+            case 'q': exit(0);
+                      break;
+            case 'c':
+            case 'C': system("cls");
+                      calculator_operations();
+                      break;
+ 
+            default : system("cls");
+ 
+    printf("\n**********You have entered unavailable option");
+    printf("***********\n");
+    printf("\n*****Please Enter any one of below available ");
+    printf("options****\n");
+                      calculator_operations();
+        }
+    }
+}
+ 
+//Function Definitions
+ 
+void calculator_operations()
+{
+    //system("cls");  use system function to clear 
+    //screen instead of clrscr();
+    printf("\n             Welcome to C calculator \n\n");
+ 
+    printf("******* Press 'Q' or 'q' to quit ");
+    printf("the program ********\n");
+    printf("***** Press 'H' or 'h' to display ");
+    printf("below options *****\n\n");
+    printf("Enter 'C' or 'c' to clear the screen and");
+    printf(" display available option \n\n");
+ 
+    printf("Enter + symbol for Addition \n");
+    printf("Enter - symbol for Subtraction \n");
+    printf("Enter * symbol for Multiplication \n");
+    printf("Enter / symbol for Division \n");
+    printf("Enter ? symbol for Modulus\n");
+    printf("Enter ^ symbol for Power \n");
+    printf("Enter ! symbol for Factorial \n\n");
+}
